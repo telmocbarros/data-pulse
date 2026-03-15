dab57b916da49248a1265bf925e0cfa2cf791ffd
Telmo Barros <somebodyceo@gmail.com>
2026-03-15 10:57:55 +0100

    removed unused functions

--- cmd/server.go
+++ cmd/server.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"mime/multipart"
 	"net/http"
-	"os"
 	"strconv"
 	"time"
 )
@@ -38,118 +37,6 @@ func main() {
 
 func health(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("healthy"))
-}
-
-func uploadDataset(w http.ResponseWriter, r *http.Request) {
-	// 1. Parse input, type multipart/form-data
-
-	// << is a bitwise operator. By putting a number to its right,
-	// you're shifting the number on its left in its binary representation
-	// by that amount of zeros
-	// Usual patterns:
-	// << 10 = ×1,024 (KiB)
-	// << 20 = ×1,048,576 (MiB)
-	// << 30 = ×1,073,741,824 (GiB)
-	r.ParseMultipartForm(10 << 20)
-
-	// 2. Retrieve file from posted form-data
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		fmt.Println("Error retrieving file: ", err)
-		http.Error(w, "Error retrieving file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	mimeType := handler.Header.Get("Content-Type")
-	if mimeType == "" {
-		fmt.Println("Invalid file type: (empty)")
-		http.Error(w, "Invalid file type", http.StatusBadRequest)
-		return
-	} else if mimeType != "text/csv" && mimeType != "application/json" {
-		fmt.Println("Invalid file type: ", mimeType)
-		http.Error(w, "Invalid file type", http.StatusBadRequest)
-		return
-	}
-
-	// 3. Write temporary file on our server
-	// %#v	a Go-syntax representation of the value
-	// (floating-point infinities and NaNs print as ±Inf and NaN)
-	fmt.Printf("File name: %+v\n", handler.Filename)
-	fmt.Printf("File size: %+v B\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
-	fmt.Printf("Content Type: %s\n", handler.Header.Get("Content-Type"))
-
-	fileBytes, err := io.ReadAll(file)
-	if err != nil {
-		fmt.Println("Error reading file: ", err)
-		http.Error(w, "Error reading file", http.StatusInternalServerError)
-		return
-	}
-
-	fileToCreate, err := os.Create("temp.txt")
-	if err != nil {
-		fmt.Println("Error creating file: ", err)
-		http.Error(w, "Error creating file", http.StatusInternalServerError)
-		return
-	}
-
-	defer fileToCreate.Close()
-
-	// 4. Return whether or not this has been succesfull
-	_, err = fileToCreate.Write(fileBytes)
-	if err != nil {
-		fmt.Println("Error writing to file: ", err)
-		http.Error(w, "Error writing to file", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
-}
-
-func uploadDatasetInChunks(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseMultipartForm(10 << 20)
-	if err != nil {
-		fmt.Println("Error parsing file: ", err)
-		http.Error(w, "Error parsing file", http.StatusBadRequest)
-		return
-	}
-
-	file, handler, err := r.FormFile("file")
-	if err != nil {
-		fmt.Println("Error retrieving file: ", err)
-		http.Error(w, "Error retrieving file", http.StatusBadRequest)
-		return
-	}
-	defer file.Close()
-
-	mimeType := handler.Header.Get("Content-Type")
-	if mimeType == "" {
-		fmt.Println("Invalid file type: (empty)")
-		http.Error(w, "Invalid file type", http.StatusBadRequest)
-		return
-	} else if mimeType != "text/csv" && mimeType != "application/json" {
-		fmt.Println("Invalid file type: ", mimeType)
-		http.Error(w, "Invalid file type", http.StatusBadRequest)
-		return
-	}
-	fileToCreate, err := os.Create("temp.txt")
-	if err != nil {
-		fmt.Println("Error creating file: ", err)
-		http.Error(w, "Error creating file", http.StatusInternalServerError)
-		return
-	}
-	defer fileToCreate.Close()
-
-	written, err := io.Copy(fileToCreate, file)
-	if err != nil {
-		fmt.Println("Error writing to file: ", err)
-		http.Error(w, "Error writing to file", http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Printf("Succesfully uploaded file: %v B\n", written)
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
 }
 
 func parseCsvFile(f multipart.File) (results [][]any, validationErrors []ValidationError, err error) {
@@ -414,6 +301,38 @@ func computeVariableType(value string) (valueType string, err error) {
 	return IS_TEXT, nil
 }
 
+func computeJsonVariableType(value any) (string, error) {
+	return computeVariableType(fmt.Sprintf("%v", value))
+}
+
+func readCsvRowAndExtractType(csvReader *csv.Reader) ([]string, []string, error) {
+	content, err := csvReader.Read()
+	if err != nil {
+		log.Println("Something went wrong reading the file: ", err)
+		return nil, nil, err
+	}
+
+	var cellTypes []string
+	for _, value := range content {
+		variableType, err := computeVariableType(value)
+		if err != nil {
+			fmt.Println("Error retrieving cell variable type: ", err)
+			return nil, nil, err
+		}
+		cellTypes = append(cellTypes, variableType)
+	}
+
+	return content, cellTypes, nil
+}
+
+func readJsonRowAndExtractType(row map[string]any) {
+	for k, v := range row {
+		if strValue, ok := v.(string); ok {
+			row[k] = parseValue(strValue)
+		}
+	}
+}
+
 func parseValue(value string) any {
 	intResult, err := strconv.ParseInt(value, 10, 64)
 	if err == nil {
@@ -445,36 +364,4 @@ func parseValue(value string) any {
 	}
 
 	return value
-}
-
-func readCsvRowAndExtractType(csvReader *csv.Reader) ([]string, []string, error) {
-	content, err := csvReader.Read()
-	if err != nil {
-		log.Println("Something went wrong reading the file: ", err)
-		return nil, nil, err
-	}
-
-	var cellTypes []string
-	for _, value := range content {
-		variableType, err := computeVariableType(value)
-		if err != nil {
-			fmt.Println("Error retrieving cell variable type: ", err)
-			return nil, nil, err
-		}
-		cellTypes = append(cellTypes, variableType)
-	}
-
-	return content, cellTypes, nil
-}
-
-func readJsonRowAndExtractType(row map[string]any) {
-	for k, v := range row {
-		if strValue, ok := v.(string); ok {
-			row[k] = parseValue(strValue)
-		}
-	}
-}
-
-func computeJsonVariableType(value any) (string, error) {
-	return computeVariableType(fmt.Sprintf("%v", value))
 }
