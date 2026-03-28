76fb9c4e6a21774aa171c5038eb2c41f2c452d15
Telmo Barros <somebodyceo@gmail.com>
2026-03-28 18:20:15 +0100

    added profiler type definitions

---
+++ internal/service/dataset_upload/profiler.go
@@ -0,0 +1,44 @@
+package service
+
+type HistogramBucket struct {
+	Min   float64
+	Max   float64
+	Count int64
+}
+
+type NumericProfiler struct {
+	ColumnName       string
+	Min              float64
+	Max              float64
+	Mean             float64
+	Median           float64
+	Stdv             float64
+	P25              float64
+	P50              float64
+	P75              float64
+	NullCount        int64
+	NullPercent      float64
+	Histogram        []HistogramBucket
+	TypeDistribution map[string]float64
+}
+
+type Ratio struct {
+	Value        float64
+	UniqueValues int64
+	TotalRows    int64
+}
+
+type Occurrence struct {
+	Value string
+	Count int64
+}
+
+type CategoryProfiler struct {
+	ColumnName         string
+	Cardinality        int64
+	UniquenessRatio    Ratio
+	NullCount          int64
+	NullPercent        float64
+	MostFrequentValues []Occurrence
+	TypeDistribution   map[string]float64
+}
