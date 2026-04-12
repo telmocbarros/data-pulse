a7147aab9d33970a2f0c6b8c49e91f14109680e8
Telmo Barros <somebodyceo@gmail.com>
2026-04-12 20:07:43 +0200

    added migration to fix the file name being too long

--- internal/repository/migrations/04-add-jobs-table.sql
+++ internal/repository/migrations/04-add-jobs-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS jobs (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     file_name VARCHAR(255) NOT NULL,
-    file_type VARCHAR(10) NOT NULL,
+    file_type VARCHAR(50) NOT NULL,
     status VARCHAR(20) NOT NULL DEFAULT 'pending',
     progress INTEGER NOT NULL DEFAULT 0,
     error TEXT,
