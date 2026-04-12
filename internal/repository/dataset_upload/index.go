9c6511be44769e2e4b3f917f40a51a18b9a99a7b
Telmo Barros <somebodyceo@gmail.com>
2026-04-12 19:13:21 +0200

    added function to upload raw file to file storage

--- internal/repository/dataset_upload/index.go
+++ internal/repository/dataset_upload/index.go
@@ -1,10 +1,13 @@
 package repository
 
 import (
+	"context"
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/google/uuid"
+	"github.com/minio/minio-go/v7"
 	"github.com/telmocbarros/data-pulse/config"
 )
 
@@ -183,6 +186,35 @@ func GetDatasetRows(tableName string) ([]map[string]any, error) {
 	}
 
 	return result, nil
+}
+
+// StoreRawFile uploads a file to MinIO under the datasets bucket.
+func StoreRawFile(datasetId string, filePath string, fileName string) error {
+	f, err := os.Open(filePath)
+	if err != nil {
+		return fmt.Errorf("unable to open file: %w", err)
+	}
+	defer f.Close()
+
+	info, err := f.Stat()
+	if err != nil {
+		return fmt.Errorf("unable to stat file: %w", err)
+	}
+
+	objectKey := fmt.Sprintf("%s/%s", datasetId, fileName)
+	_, err = config.FileStorage.PutObject(
+		context.Background(),
+		config.DatasetsBucket,
+		objectKey,
+		f,
+		info.Size(),
+		minio.PutObjectOptions{},
+	)
+	if err != nil {
+		return fmt.Errorf("unable to upload file to MinIO: %w", err)
+	}
+
+	return nil
 }
 
 func mapToDatabase(value string) string {
